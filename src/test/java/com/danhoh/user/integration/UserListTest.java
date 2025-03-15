bf604f9bede0081c14ec664d30816df39ab0de55
Daniil Khokhlov <danhoh228@gmail.com>
2025-03-15 18:47:00 +0300

    dummy another123

--- src/test/java/com/danhoh/user/integration/UserListTest.java
+++ src/test/java/com/danhoh/user/integration/UserListTest.java
@@ -48,7 +48,7 @@ public class UserListTest extends BaseIntegrationTest {
     }
 
     @Test
-    void successCase() {
+    void successCase() { //111
         given()
                 .spec(requestSpecification)
                 .body(User.builder().build())
